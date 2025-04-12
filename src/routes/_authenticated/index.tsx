9a0ee1df110249c35309020018f2689ad9ffbb0f
phuocnd <ducphuoc.t9@gmail.com>
2025-04-12 22:40:34 +0700

    fix: add sort filter

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import Tasks from '@/features/tasks'
+import Fees from '@/features/fees'
 
 export const Route = createFileRoute('/_authenticated/')({
-  component: Tasks,
+  component: Fees,
 })
